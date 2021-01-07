36c3adad208ea4353abecbf608ba2c5434dcf28f
ralfitii420 <ralfs.ralfis@gmail.com>
2021-01-07 10:35:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print ("Sveika, pasaule!")
+print (2*2)
+print (2*2, 2*3, 2*4, "Pritons")
+print (f"Ja saskaitīsim 5 ar 7, iegūsim {5+7}.") #kombinētā izdruka - teksts un aprēķins
+print ("Sveika, " + "pasaule!")
+print ("Ralfs")
+print ("Ralfs")
+print ("Ralfs")
+print ("Ralfs")
+print ("Ralfs "*5)
+print ("Šogad būs tik daudz sekundes - ") 
+print (365*24*60*60)
+print (0,1+0,2-0,2)
