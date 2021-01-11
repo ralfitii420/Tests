0d93f035ee9dd31e7c5187c21508752aeaa97903
ralfitii420 <ralfs.ralfis@gmail.com>
2021-01-11 11:28:36 +0000

    Nezināmos burtus iemācījos aprēķināt

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ print ("Ralfs "*5)
 print ("Šogad būs tik daudz sekundes - ") 
 print (365*24*60*60)
 print (0,1+0,2-0,2) #pirmā pritona pieredze
+print (1/2)
+#mainīgie
+a= 5
+print (a) 
+print (a+a)
+a=a+a #zīme = nozīmē peisķiršanu
+print (a)
+#datu ievade - INPUT
