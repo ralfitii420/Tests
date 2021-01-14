e95ecc1abf707fa54e07ac14a8f03b77f20977ac
ralfitii420 <ralfs.ralfis@gmail.com>
2021-01-14 10:40:38 +0000

    cits folderis

---
+++ Izdruka.py
@@ -0,0 +1,21 @@
+print ("Sveika, pasaule!")
+print (2*2)
+print (2*2, 2*3, 2*4, "Pritons")
+print (f"Ja saskaitīsim 5 ar 7, iegūsim {5+7}.") #kombinētā izdruka - teksts un aprēķins
+print ("Sveika, " + "pasaule!")
+print ("Ralfs")
+print ("Ralfs")
+print ("Ralfs")
+print ("Ralfs")
+print ("Ralfs "*5)
+print ("Šogad būs tik daudz sekundes - ") 
+print (365*24*60*60)
+print (0,1+0,2-0,2) #pirmā pritona pieredze
+print (1/2)
+#mainīgie
+a= 5
+print (a) 
+print (a+a)
+a=a+a #zīme = nozīmē peisķiršanu
+print (a)
+#datu ievade - INPUT
---
+++ Virknes.py
@@ -0,0 +1,40 @@
+"""
+vards =input("Kā tevi sauc? ")
+print(f"Tavs vārds ir {vards}.")
+
+gadi=int(input("Cik tev gadu?"))
+print(f"Tev ir {gadi} gadi.")
+dzimsGads=2021-gadi
+print(f"Tavs dzimšanas gads ir {szimsGadi}.")
+"""
+#uzdevums
+
+celsijs = int(input("Ieraksti kāda temperatūra tev ir."))
+fareinheiti = celsijs * 9 / 5 + 32
+print(f"Temperatūra {celsijs} grādi pēc Celsija skalas ir {fareinheiti} grādi pēc Fareinheita skalas ")
+
+platums=float(input("platums:"))
+garums=float(input("garums: "))
+augstums=float(input("augstums: "))
+print(f"tilpums ir {platums*augstums*garums}.")
+
+#Strings  (rakstzīmju virknes*)
+print("sveiki")
+print('sveiki')
+print("I'm going on a run")
+print('Arī šis ir "risinājums"')
+print("sveika, \npasaule!") #izdrukā divās rindās
+print("Sveika, \tpasaule!") #izdrukā ar tabulācijas atkāpi
+
+#String garums - len ()
+print(len("sveiki"))
+print(len("Ko tu domā?"))
+
+# [sākums:beigas:solis]
+myString="Sveika, pasaule!"
+print(myString)
+print(myString[0]) #izdrukā pirmo rakstzīmi
+print(myString[8])#izdrukā 9.rakstzīmi
+print(myString[13])#izdrukā 14.rakstzīmi
+print(myString[-3])#izdrukā 14.rakstzīmi
+print (myString[-1])#izdrukā pēdējo rakstzīmi
